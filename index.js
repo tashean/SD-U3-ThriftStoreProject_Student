4d90193b6bf2e1a136eacd015c3d1f4b2dc611a5
tashean <tasheanestor@icloud.com>
2023-11-20 03:45:27 -0500

    Fixed URL background image & worked on Sale Tax

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //? This array is not to be changed.
-const salesTax = [
+/* const salesTax = [
     {state: 'Alabama', tax: .04},
     {state: 'Alaska', tax: .00},
     {state: 'Arizona', tax: .056},
@@ -51,34 +51,129 @@ const salesTax = [
     {state: 'West Virginia', tax: .06},
     {state: 'Wisconsin', tax: .05},
     {state: 'Wyoming', tax: .04},
-];
+]; */
 
-//! Classes
+class Product {
+    constructor(upc, name, type, purchasePrice, quantity = 1, marketPrice = 0) {
+        this.upc = upc;
+        this.name = name;
+        this.type = type;
+        this.purchasePrice = purchasePrice;
+        this.quantity = quantity;
+        this.marketPrice = marketPrice;
+    }
+    }
 
+    class Store {
+    constructor(name, city, state, saleTax) {
+        this.name = name;
+        this.location = {
+        city: city,
+        state: state,
+        };
+        this.salesTax = saleTax;
+        this.inventory = [];
+      this.balance = 200; // Starting balance
+        this.expenses = 0;
+        this.profit = 0;
+        this.paidTax = 0;
 
-//! CREATE STORES
-// Generate 3 different stores, each in a different state.
+      // Set sales tax based on the provided array
+        this.setSalesTax();
+    }
 
-//! Inventory
+    setSalesTax() {
+        const stateInfo = salesTax.find((info) => info.state === this.location.state);
+        if (stateInfo) {
+        this.salesTax = stateInfo.tax;
+        } else {
+        console.error(`Error: Sales tax information not found for ${this.location.state}`);
+        }
+    }
 
+    addToInventory(product, markupPercentage) {
+        const existingProduct = this.inventory.find((item) => item.upc === product.upc);
 
-//! Stocking
+        if (existingProduct) {
+        // Item already exists, update quantity
+        existingProduct.quantity += product.quantity;
+        } else {
+        // Item doesn't exist, add to inventory
+        const marketPrice = product.purchasePrice + product.purchasePrice * markupPercentage;
+        product.marketPrice = marketPrice;
+        this.inventory.push(product);
+        this.balance -= product.purchasePrice * product.quantity; // Update store balance
+        }
+    }
 
-//* First Store
+    sellItem(upc, quantity) {
+        const soldItem = this.inventory.find((item) => item.upc === upc);
+  
+        if (soldItem && soldItem.quantity >= quantity) {
+        // Calculate profit
+        const profit = quantity * (soldItem.marketPrice - soldItem.purchasePrice);
+        this.profit += profit;
+  
+        // Update balance
+        this.balance += quantity * soldItem.marketPrice;
 
-//* Second Store
+        // Update inventory
+        soldItem.quantity -= quantity;
 
-//* Third Store
+        // Update paid tax
+        const taxAmount = (soldItem.marketPrice * this.salesTax) / 100;
+        this.paidTax += taxAmount;
 
-//! Selling
+        console.log(`Sold ${quantity} ${soldItem.name}(s) from ${this.name}. Profit: $${profit}`);
+        } else if (soldItem) {
+        console.error(`Error: Insufficient quantity of ${soldItem.name} in stock.`);
+        } else {
+        console.error(`Error: Item with UPC ${upc} not found in ${this.name}'s inventory.`);
+        }
+    }
+    }
 
-//* First Store
+  //* Store # 1: Thrift Haven
+    const store1 = new Store("Thrift Haven", "Houston", "Texas", salesTax);
+    const item1 = new Product(1, "Vintage Chair", "Furniture", 20, 2);
+    const item2 = new Product(2, "Classic Book", "Books", 5, 5);
+    const item3 = new Product(3, "Toy Train", "Toys", 10, 3);
 
-//* Second Store
+  store1.addToInventory(item1, 0.2); // 20% markup
+  store1.addToInventory(item2, 0.3); // 30% markup
+  store1.addToInventory(item3, 0.25); // 25% markup
 
-//* Third Store
+  //* Store # 2: Timeless Treasures
+    const store2 = new Store("Timeless Treasures", "Seattle", "Washington", salesTax);
+    const item4 = new Product(4, "Kitchen Blender", "Kitchenware", 30, 2);
+    const item5 = new Product(5, "Board Game", "Toys", 15, 2);
 
-//! Testing
-/* 
-    Simply console log each store to check the completed details.
-*/
+  store2.addToInventory(item4, 0.15); // 15% markup
+  store2.addToInventory(item5, 0.2); // 20% markup
+
+  //* Store # 3: Rarities Style Realm
+    const store3 = new Store("Rarities Style Realm", "New York City", "New York", salesTax);
+    const item6 = new Product(6, "Antique Mirror", "Decor", 50, 1);
+    const item7 = new Product(7, "Collectible Vinyl", "Music", 25, 3);
+
+  store3.addToInventory(item6, 0.18); // 18% markup
+  store3.addToInventory(item7, 0.25); // 25% markup
+
+  //* Selling
+
+  //* Store # 1: Thrift Haven
+  store1.sellItem(1, 1); // Sell 1 Vintage Chair
+  store1.sellItem(2, 3); // Try to sell 3 Classic Books (insufficient quantity)
+
+  //* Store # 2: Timeless Treasures
+  store2.sellItem(4, 2); // Sell 2 Kitchen Blenders
+  store2.sellItem(5, 3); // Try to sell 3 Board Games (insufficient quantity)
+
+  //* Store # 3: Rarities Style Realm
+  store3.sellItem(6, 1); // Sell 1 Antique Mirror
+  store3.sellItem(7, 4); // Try to sell 4 Collectible Vinyls (insufficient quantity)
+
+  //* Testing
+    console.log("Store 1:", store1);
+    console.log("Store 2:", store2);
+    console.log("Store 3:", store3);
